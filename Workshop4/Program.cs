6cc1dcb9b83bd1bbef8652fe7154e6c3c04a8911
Karuna1630 <karunagiri481@gmail.com>
2025-11-17 11:54:59 +0545

    Add project files.

---
+++ Workshop4/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Workshop4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.name = "Karuna Giri";
+            student1.age = 20;
+            student1.id = 101;
+            Console.WriteLine(student1.name);
+            Student student2 = new Student();
+            Console.WriteLine(student1.age);
+        }
+    }
+}
---
+++ Workshop4/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop4
+{
+    internal class Student
+    {
+        
+        public int age;
+        public int id;
+        public string? name;
+        
+        public static int totalStudents;
+    }
+}
