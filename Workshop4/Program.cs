66de719ceba1e6ebd22ad1c074f842a4f3c6ab8b
Karuna1630 <karunagiri481@gmail.com>
2025-11-17 11:59:10 +0545

    Task1: Creating class, object & fields

--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -4,13 +4,19 @@
     {
         static void Main(string[] args)
         {
+            // Creating a student object
             Student student1 = new Student();
             student1.name = "Karuna Giri";
             student1.age = 20;
             student1.id = 101;
             Console.WriteLine(student1.name);
-            Student student2 = new Student();
+            Console.WriteLine(student1.id);
             Console.WriteLine(student1.age);
+
+            // Creating another student object
+            Student student2 = new Student();
+            student2.name = "Anil Thapa";   
+            Console.WriteLine(student2.age);
         }
     }
 }
