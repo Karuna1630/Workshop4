153e2fa43cfd8c0debb80d976e3c856899958df1
Karuna1630 <karunagiri481@gmail.com>
2025-11-18 07:21:32 +0545

    Task 2: creating calculatior

---
+++ Workshop4/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop4
+{
+    internal class Calculator
+    {
+        // Void method
+        public void PrintWelcome()
+        {
+            Console.WriteLine("Welcome to the Calculator!");
+        }
+
+        // Method that returns addition
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        // Multiply method with optional parameter
+        public int Multiply(int num1, int num2 = 1)
+        {
+            return num1 * num2;
+        }
+    }
+}
--- Workshop4/Program.cs
+++ Workshop4/Program.cs
@@ -4,32 +4,50 @@
     {
         static void Main(string[] args)
         {
-            // Creating first student object
-            Student student1 = new Student();
-            student1.id = 101;
-            student1.name = "Karuna Giri";
-            student1.age = 20;
+            // Create object
+            Calculator calc = new Calculator();
 
-            // Displaying details of the first student
-            Console.WriteLine("Student 1:");
-            Console.WriteLine(student1.name);
-            Console.WriteLine(student1.id);
-            Console.WriteLine(student1.age);
+            // Call and print welcome message
+            calc.PrintWelcome();
 
-            // Creating second student object
-            Student student2 = new Student();
-            student2.id = 102;
-            student2.name = "Anil Thapa";   
-            student2.age = 20;
+            //calling calculator methods
+            Console.WriteLine("Addition: " + calc.Add(5, 3));
+            Console.WriteLine("Multiplication: " + calc.Multiply(5, 3));
 
-            // Displaying details of the second student
-            Console.WriteLine("Student 2:");
-            Console.WriteLine(student2.name);
-            Console.WriteLine(student1.id);
-            Console.WriteLine(student1.age);
+            // Call Multiply method (using default value for num2)
+            Console.WriteLine("Multiplication: " + calc.Multiply(5));
 
-            // Printing static field
-            Console.WriteLine("Total Students: " + Student.totalStudents);
+         
+
+
+
+
+            //// Creating first student object
+            //Student student1 = new Student();
+            //student1.id = 101;
+            //student1.name = "Karuna Giri";
+            //student1.age = 20;
+
+            //// Displaying details of the first student
+            //Console.WriteLine("Student 1:");
+            //Console.WriteLine(student1.name);
+            //Console.WriteLine(student1.id);
+            //Console.WriteLine(student1.age);
+
+            //// Creating second student object
+            //Student student2 = new Student();
+            //student2.id = 102;
+            //student2.name = "Anil Thapa";   
+            //student2.age = 20;
+
+            //// Displaying details of the second student
+            //Console.WriteLine("Student 2:");
+            //Console.WriteLine(student2.name);
+            //Console.WriteLine(student1.id);
+            //Console.WriteLine(student1.age);
+
+            //// Printing static field
+            //Console.WriteLine("Total Students: " + Student.totalStudents);
 
 
 
